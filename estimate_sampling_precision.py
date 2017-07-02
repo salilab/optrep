f731c055e012ee25ab23e541de87576fcc64a927
shruthi <shruthi.viswanath@gmail.com>
2017-07-01 17:45:12 -0700

    Added wrapper scripts

--- estimate_sampling_precision.py
+++ estimate_sampling_precision.py
@@ -7,15 +7,18 @@ import argparse
 
 def parse_args():
     
-    parser = argparse.ArgumentParser(description="Get the sampling precision per bead and identify imprecise beads. Usage: estimate_sampling_precision.py -c <components_to_update. e.g. [("A","A_1"),("B","B_1")], list of protein, domain tuples. > -rd <run_directory_for_sampling> -gs <grid_size> -xs <xscale>. Flag -h for more details.")
-    parser.add_argument("-pl","--protein_list",dest="proteins_to_update",nargs="*",type=string,help="proteins to calcualte precision on")
+    parser = argparse.ArgumentParser(description="Get the sampling precision per bead and identify imprecise beads. Usage: estimate_sampling_precision.py -pl <protein list: e.g. A B> (Number of elements in protein and domain lists should match.)  -dl <domain list: e.g. A_1 B_1> -rd <run_directory_for_sampling> -gs <grid_size> -xs <xscale>. Flag -h for more details.")
+    parser.add_argument("-pl","--protein_list",dest="proteins_to_update",nargs="+",type=string,help="proteins to calcualte precision on")
     
-    parser.add_argument("-dl","--domain_list",dest="domains_to_update",nrgs="*",type=string,help="domain to calculate precision on components to calculate precisions on.")
+    parser.add_argument("-dl","--domain_list",dest="domains_to_update",nrgs="+",type=string,help="domain to calculate precision on")
+
     parser.add_argument("-rd","--run_directory",dest="run_directory",help="directory in which sampling results are stored") 
     
     parser.add_argument("-gs","--grid_size",dest="grid_size",help="Size of grid for clustering and determining sampling precision") 
     
     parser.add_argument("-xs","--xscale",dest="xscale",help="Sampling precision can be utmost xscale times the representation precision plus 2.0") 
+    
+    parser.add_argument("-o","--outfile",dest="output_file",help="File where precision values and identity of imprecise beads is written")
                  
     result = parser.parse_args()
  
@@ -29,9 +32,11 @@ def estimate_sampling_precision():
     
     ### list of (protein, domain) tuples corresponding to topology file
     ### could be changed to PMI selection later
-    components_to_update=[("A","A_1")] #specific to application
+    components_to_update=[]
+    for prot,dom in zip(arg.proteins_to_update,arg,domains_to_update):
+        components_to_update.append((prot,dom))
         
-    spe=IMP.optrep.SamplingPrecisionEstimator.SamplingPrecisionEstimator(os.path.join(arg.run_dir,"good_scoring_models"),arg.components_to_update) # the components to calculate precision are the 
+    spe=IMP.optrep.SamplingPrecisionEstimator.SamplingPrecisionEstimator(os.path.join(arg.run_dir,"good_scoring_models"),components_to_update) # the components to calculate precision are the 
     ## same as the components to update
         
     spe.load_coordinates_and_bead_sizes_from_model_files()
