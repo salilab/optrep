bb448769ed6925fa2735c45cd4906fbb65885406
shruthi <shruthi.viswanath@gmail.com>
2018-10-31 11:30:43 -0700

    added plot for generating beadmaps colored by sampling precision

--- utilities/plot_beadmaps_figure2.py
+++ utilities/plot_beadmaps_figure2.py
@@ -14,6 +14,9 @@ import matplotlib.pyplot as plt
 import numpy
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Rectangle
+from matplotlib import gridspec
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def parse_args():
     
@@ -145,56 +148,81 @@ def plot_beadmaps():
          
     # changing the font sizes of axes and so on
     # need to put it in the front of the file
-    matplotlib.rcParams["xtick.labelsize"] = 16 
-      
+    matplotlib.rcParams["xtick.labelsize"] = 24 
+     
+    # for getting colorbar  
+    #fig = plt.figure(figsize=(15, 4))
+    #gs = gridspec.GridSpec(15, 1)
+
     fig = plt.figure(figsize=(14, 4))
- 
+    gs = gridspec.GridSpec(12, 1)
+
     # first add the xlinks to show where the data is along the sequence
-    ax1 = fig.add_subplot(6,1,1)
+    #ax1 = fig.add_subplot(7,1,1)
+    ax1 = fig.add_subplot(gs[0,0])
     ax1.bar(residue_range,xlink_bars,width=1.0,align='edge',color='black')
     ax1.set_xlim(min(residue_range), max(residue_range)+1)
     ax1.set_xticks([])
     ax1.set_yticks([])
+    
+    # get an empty subplot
+    #ax2 = fig.add_subplot(7,1,2)
+    ax2  = fig.add_subplot(gs[1,0])
+    ax2.set_visible(False)
         
-    ax2 = fig.add_subplot(6,1,2)
-    ax2.imshow(precision_residues['1'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
-    ax2.vlines(bead_boxes['1'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=0.1)
-    ax2.set_xlim(min(residue_range), max(residue_range)+1)
-    ax2.set_xticks([])
-    ax2.set_yticks([])
+    #ax3 = fig.add_subplot(7,1,3)
+    ax3 = fig.add_subplot(gs[2:4,0])
+    ax3.imshow(precision_residues['1'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
+    ax3.vlines(bead_boxes['1'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=1.5)
+    ax3.set_xlim(min(residue_range), max(residue_range)+1)
+    ax3.set_xticks([])
+    ax3.set_yticks([])
     
     # Make sure vlines, precisions and the x axes are all sync'ed
     
     #normalize precision over full heatmap. Can you compare precisions of different runs? You cannot but the numerical values need to be compared, so yes. Make it uniform. 
     
-    ax3 = fig.add_subplot(6,1,3)
-    ax3.imshow(precision_residues['5'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
-    ax3.vlines(bead_boxes['5'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=0.1)
-    ax3.set_xlim(min(residue_range), max(residue_range)+1)
-    ax3.set_xticks([])
-    ax3.set_yticks([])
-    
-    ax4 = fig.add_subplot(6,1,4)
-    ax4.imshow(precision_residues['10'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
-    ax4.vlines(bead_boxes['10'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=0.1)
+    #ax4 = fig.add_subplot(7,1,4)
+    ax4 = fig.add_subplot(gs[4:6,0])
+    ax4.imshow(precision_residues['5'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
+    ax4.vlines(bead_boxes['5'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=1.5)
     ax4.set_xlim(min(residue_range), max(residue_range)+1)
     ax4.set_xticks([])
     ax4.set_yticks([])
-  
-   
-    ax5 = fig.add_subplot(6,1,5)
-    ax5.imshow(precision_residues['20'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
-    ax5.vlines(bead_boxes['20'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=0.1)
+    
+    #ax5 = fig.add_subplot(7,1,5)
+    ax5 = fig.add_subplot(gs[6:8,0])
+    ax5.imshow(precision_residues['10'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
+    ax5.vlines(bead_boxes['10'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=1.5)
     ax5.set_xlim(min(residue_range), max(residue_range)+1)
     ax5.set_xticks([])
     ax5.set_yticks([])
+  
    
-    ax6 = fig.add_subplot(6,1,6)    
-    ax6.imshow(precision_residues['30'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
-    ax6.vlines(bead_boxes['30'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=0.1)
+    #ax6 = fig.add_subplot(7,1,6)
+    ax6 = fig.add_subplot(gs[8:10,0])
+    ax6.imshow(precision_residues['20'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
+    ax6.vlines(bead_boxes['20'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=1.5)
     ax6.set_xlim(min(residue_range), max(residue_range)+1)
-    ax6.set_xticks(numpy.arange(min(residue_range), max(residue_range)+1, 20))
+    ax6.set_xticks([])
     ax6.set_yticks([])
+   
+    #ax7 = fig.add_subplot(7,1,7)    
+    ax7 = fig.add_subplot(gs[10:12,0])
+    color_image = ax7.imshow(precision_residues['30'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
+    ax7.vlines(bead_boxes['30'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=1.5)
+    ax7.set_xlim(min(residue_range), max(residue_range)+1)
+    ax7.set_xticks(numpy.arange(min(residue_range), max(residue_range)+1, 20))
+    ax7.set_yticks([])
+    
+    # this is for getting the color bar only
+    ## empty subplot
+    #ax8  = fig.add_subplot(gs[12:14,0])
+    #ax8.set_visible(False)
+    
+    #ax9 = fig.add_subplot(gs[14:15,0])
+    #cbar = fig.colorbar(color_image, cax=ax9, ticks=[min_precision, (min_precision+max_precision)/2.0, max_precision], orientation='horizontal',fraction=1.5,shrink=0.5,pad=1.0,aspect=100)
+    #cbar.set_ticklabels(['High', 'Medium', 'Low'])  # horizontal colorbar
     
     plt.tight_layout()
    
