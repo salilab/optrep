8358a20026de06515fc9dc175e8d1906cfb9fcd5
Ben Webb <ben@salilab.org>
2023-10-15 16:08:53 -0700

    We now need Boost headers to build with IMP on Python 3

--- tools/setup_travis.sh
+++ tools/setup_travis.sh
@@ -13,8 +13,13 @@ temp_dir=$(mktemp -d)
 
 cd ${temp_dir}
 
+if [ ${python_version} = "2.7" ]; then
+  BOOST=""
+else
+  BOOST="libboost-devel"
+fi
 conda config --remove channels defaults  # get conda-forge, not main, packages
-conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} pip scipy matplotlib imp-nightly gxx_linux-64 eigen cereal swig cmake
+conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} pip scipy matplotlib imp-nightly ${BOOST} gxx_linux-64 eigen cereal swig cmake
 eval "$(conda shell.bash hook)"
 conda activate python${python_version}
 pip install pytest-cov coverage
