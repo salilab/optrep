483ac1e797e025c1f508f843e0ea1281adeb67b5
Ben Webb <benmwebb@gmail.com>
2026-03-31 16:52:04 -0700

    flake8 fixes

--- pyext/src/select_good_scoring_models.py
+++ pyext/src/select_good_scoring_models.py
@@ -1,41 +1,67 @@
-
 import IMP
 import IMP.optrep
 import IMP.optrep.BeadMapBuilder
 import IMP.optrep.GoodScoringModelSelector
 import IMP.optrep.SamplingPrecisionEstimator
-import os,sys,string,math
 import argparse
 
+
 def parse_args():
-    
-    parser = argparse.ArgumentParser(description="List and extract good-scoring models from a set of sampling runs. Example of usage: select_good_scoring_models.py -rd <run_directory_for_sampling> -rp <run_prefix> -gs <grid_size>. Flag -h for more details.")
-    
-    parser.add_argument("-rd","--run_directory",dest="run_dir",help="directory in which sampling results are stored")
-    
-    parser.add_argument("-rp","--run_prefix",dest="run_prefix",help="prefix of runs") 
-                        
-    parser.add_argument("-cl","--criteria_list",nargs='+',type=str,dest="criteria_list",help="list of criteria")
-    parser.add_argument("-kl","--keywords_list",nargs='+',type=str,dest="keywords_list",help="list of stat file keywords")
-    parser.add_argument("-agl","--aggregate_lower_thresholds",nargs='+',type=float,dest="aggregate_lower_thresholds",help="aggregate lower threshold values")
-    parser.add_argument("-aul","--aggregate_upper_thresholds",nargs='+',type=float,dest="aggregate_upper_thresholds",help="aggregate upper threshold values")
-    parser.add_argument("-mlt","--member_lower_thresholds",nargs='+',type=float,dest="member_lower_thresholds",help="member lower thresholds")
-    parser.add_argument("-mut","--member_upper_thresholds",nargs='+',type=float,dest="member_upper_thresholds",help="member upper thresholds")
+    parser = argparse.ArgumentParser(
+        description="List and extract good-scoring models from a set of "
+        "sampling runs. Example of usage: select_good_scoring_models.py "
+        "-rd <run_directory_for_sampling> -rp <run_prefix> -gs <grid_size>. "
+        "Flag -h for more details.")
+
+    parser.add_argument(
+        "-rd", "--run_directory", dest="run_dir",
+        help="directory in which sampling results are stored")
+
+    parser.add_argument(
+        "-rp", "--run_prefix", dest="run_prefix", help="prefix of runs")
+
+    parser.add_argument(
+        "-cl", "--criteria_list", nargs='+', type=str,
+        dest="criteria_list", help="list of criteria")
+    parser.add_argument(
+        "-kl", "--keywords_list", nargs='+', type=str, dest="keywords_list",
+        help="list of stat file keywords")
+    parser.add_argument(
+        "-agl", "--aggregate_lower_thresholds", nargs='+', type=float,
+        dest="aggregate_lower_thresholds",
+        help="aggregate lower threshold values")
+    parser.add_argument(
+        "-aul", "--aggregate_upper_thresholds", nargs='+', type=float,
+        dest="aggregate_upper_thresholds",
+        help="aggregate upper threshold values")
+    parser.add_argument(
+        "-mlt", "--member_lower_thresholds", nargs='+', type=float,
+        dest="member_lower_thresholds", help="member lower thresholds")
+    parser.add_argument(
+        "-mut", "--member_upper_thresholds", nargs='+', type=float,
+        dest="member_upper_thresholds", help="member upper thresholds")
 
     result = parser.parse_args()
- 
+
     return result
 
-    
+
 def select_good_scoring_models():
-     # Authors: Shruthi Viswanath
-     
+    # Authors: Shruthi Viswanath
+
     # process input
-    arg=parse_args()
-    
-    gsms=IMP.optrep.GoodScoringModelSelector.GoodScoringModelSelector(arg.run_dir,arg.run_prefix)
-        
-    gsms.get_good_scoring_models(criteria_list=arg.criteria_list,keywords_list=arg.keywords_list,aggregate_lower_thresholds=arg.aggregate_lower_thresholds,aggregate_upper_thresholds=arg.aggregate_upper_thresholds,member_lower_thresholds=arg.member_lower_thresholds,member_upper_thresholds=arg.member_upper_thresholds)
-        
-if __name__ == "__main__" :
+    arg = parse_args()
+
+    gsms = IMP.optrep.GoodScoringModelSelector.GoodScoringModelSelector(
+        arg.run_dir, arg.run_prefix)
+
+    gsms.get_good_scoring_models(
+        criteria_list=arg.criteria_list, keywords_list=arg.keywords_list,
+        aggregate_lower_thresholds=arg.aggregate_lower_thresholds,
+        aggregate_upper_thresholds=arg.aggregate_upper_thresholds,
+        member_lower_thresholds=arg.member_lower_thresholds,
+        member_upper_thresholds=arg.member_upper_thresholds)
+
+
+if __name__ == "__main__":
     select_good_scoring_models()
--- pyext/src/set_next_beadmap.py
+++ pyext/src/set_next_beadmap.py
@@ -3,70 +3,89 @@ import IMP.optrep
 import IMP.optrep.BeadMapBuilder
 import IMP.optrep.GoodScoringModelSelector
 import IMP.optrep.SamplingPrecisionEstimator
-import os,sys,string,math
 import argparse
 
-def parse_args():
-    
-    parser = argparse.ArgumentParser(description="Get the next iteration's representation (bead map), based on results (precision) of the previous iteration. Example of usage: (1) When creating a new beadmap: set_next_representation.py -u create -r 1 -tf <topology_file> (2) When you have previously created a beadmap and need to update it based on the results of sampling:  set_next_representation.py -u update -r <next_resolution> -bmf <previous_beadmap_file> -pf <precions_file>.Flag -h for more details.")
-    
-    parser.add_argument("-u","--usage",dest="usage",required=True,type=str,help="Say create for creating a beadmap from scratch from the topology file and update if we are in a future iteration and are updating a beadmap")
 
-    parser.add_argument("-r","--next_resolution",dest="next_resolution",required=True,type=int,help="The coarse-graining, i.e. maximum number of residues per bead to be tried in the next iteration.")
-    
-    parser.add_argument("-tf","--topology_file",action="store",dest="topology_file",help="initial topology file") 
-    
-    parser.add_argument("-bmf","--previous_bead_map_file",action="store",dest="previous_bead_map_file",help="bead map file created in previous iteration")  
-                     
-    parser.add_argument("-pf","--precisions_file",action="store",dest="precisions_file",help="file containing identity of imprecise beads") 
-    
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Get the next iteration's representation (bead map), "
+        "based on results (precision) of the previous iteration. Example of "
+        "usage: (1) When creating a new beadmap: set_next_representation.py "
+        "-u create -r 1 -tf <topology_file> (2) When you have previously "
+        "created a beadmap and need to update it based on the results of "
+        "sampling:  set_next_representation.py -u update -r <next_resolution> "
+        "-bmf <previous_beadmap_file> -pf <precions_file>."
+        "Flag -h for more details.")
+
+    parser.add_argument(
+        "-u", "--usage", dest="usage", required=True, type=str,
+        help="Say create for creating a beadmap from scratch from the "
+        "topology file and update if we are in a future iteration and are "
+        "updating a beadmap")
+
+    parser.add_argument(
+        "-r", "--next_resolution", dest="next_resolution", required=True,
+        type=int,
+        help="The coarse-graining, i.e. maximum number of residues per bead "
+        "to be tried in the next iteration.")
+
+    parser.add_argument(
+        "-tf", "--topology_file", action="store", dest="topology_file",
+        help="initial topology file")
+
+    parser.add_argument(
+        "-bmf", "--previous_bead_map_file", action="store",
+        dest="previous_bead_map_file",
+        help="bead map file created in previous iteration")
+
+    parser.add_argument(
+        "-pf", "--precisions_file", action="store", dest="precisions_file",
+        help="file containing identity of imprecise beads")
+
     result = parser.parse_args()
- 
+
     return result
 
-    
+
 def set_next_beadmap():
-     # Authors: Shruthi Viswanath
-     
+    # Authors: Shruthi Viswanath
+
     ''' Set the representation for the next iteration of CG.
     '''
-  
+
     # process input
-    args=parse_args()
-    
-  
-    if args.usage=="create": 
-        
+    args = parse_args()
+
+    if args.usage == "create":
+
         # first iteration. Get bead map for the highest resolution beads.
-        bmb=IMP.optrep.BeadMapBuilder.BeadMapBuilder()
-        
-        #TODO Note : it assumes that we use the same starting resolution for all proteins which may not be true! 
-        bmb.set_bead_map_from_topology_file(topology_file=args.topology_file,resolution=args.next_resolution) 
-           
+        bmb = IMP.optrep.BeadMapBuilder.BeadMapBuilder()
+
+        # TODO Note : it assumes that we use the same starting resolution
+        # for all proteins which may not be true!
+        bmb.set_bead_map_from_topology_file(
+            topology_file=args.topology_file, resolution=args.next_resolution)
+
     else:
-        
-        bmb=IMP.optrep.BeadMapBuilder.BeadMapBuilder()
-        
+
+        bmb = IMP.optrep.BeadMapBuilder.BeadMapBuilder()
+
         bmb.set_bead_map_from_beadmap_file(args.previous_bead_map_file)
-        
+
         bmb.set_imprecise_beads_from_file(args.precisions_file)
-        
-        #TODO Note : it assumes that we use the same resolution for all proteins which may not be true!
+
+        # TODO Note : it assumes that we use the same resolution for all
+        # proteins which may not be true!
         updated = bmb.update_all_bead_maps(int(args.next_resolution))
-      
-        # make sure the proteins for which we are not changing representation remain exactly same as before
+
+        # make sure the proteins for which we are not changing representation
+        # remain exactly same as before
         if not updated:
             print("Process converged.")
             return
-            
-    #next_iteration_sampling_dir = "r"+str(args.next_resolution)
-    
-    #os.mkdir(next_iteration_sampling_dir)
-    
-    ##Irrespective of whether we are creating a new bead map or updating an existing one, write the bead map to a file
-    #bmb.write_bead_map_to_file(os.path.join(next_iteration_sampling_dir,'bead_map_'+str(args.next_resolution)+'.txt'))
-    
+
     bmb.write_bead_map_to_file('bead_map_'+str(args.next_resolution)+'.txt')
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     set_next_beadmap()
