1656863c5678f8358cea59f07707e68c7a92810d
Ben Webb <benmwebb@gmail.com>
2026-04-01 12:18:48 -0700

    flake8 fixes

--- utilities/get_stats.py
+++ utilities/get_stats.py
@@ -1,71 +1,97 @@
-import IMP
-import IMP.rmf
-import RMF
-import IMP.atom
-import os,sys,math
-import numpy as np
+import os
+import sys
 import glob
-import datetime
 import subprocess
 import stats_helper
 
-##################
-#### main
-#################
+# main
 
 expt = sys.argv[1]
 
 bio_system = sys.argv[2]
 
-required_representation =sys.argv[3]
+required_representation = sys.argv[3]
 
 expts_dir = sys.argv[4]
 
-config_file = os.path.join(os.path.expanduser('~'),"optrep/input/"+bio_system,bio_system+".config."+expt)
+config_file = os.path.join(os.path.expanduser('~'), "optrep/input/"+bio_system,
+                           bio_system+".config."+expt)
 
 configs_dict = stats_helper.parse_config_file(config_file)
 
-out_file=open(os.path.join(os.path.expanduser('~'),"optrep/expts/stats",bio_system+".expt"+expt+".res"+required_representation+".stats.txt"),"w")
+out_file = open(os.path.join(
+    os.path.expanduser('~'), "optrep/expts/stats",
+    bio_system+".expt"+expt+".res"+required_representation+".stats.txt"), "w")
 
-os.chdir(os.path.join(expts_dir,'expt'+expt,bio_system))
+os.chdir(os.path.join(expts_dir, 'expt'+expt, bio_system))
 
 if required_representation == "max":
-    optimalRepresentationDir = 'r'+str(max([int(cg.lstrip('r')) for cg in glob.glob('r*')]))
+    optimalRepresentationDir = 'r'+str(max([int(cg.lstrip('r'))
+                                            for cg in glob.glob('r*')]))
 
 else:
     optimalRepresentationDir = 'r'+required_representation
-    
+
 os.chdir(optimalRepresentationDir)
 
 # 1. Get sampling efficiency
-machine_run_on = subprocess.check_output(["awk","$1=="+expt+" && $2==\""+bio_system+"\" {print $3}",os.path.join(os.path.expanduser('~'),"optrep/expts/info.expts")]).strip()
+machine_run_on = subprocess.check_output(
+    ["awk", "$1=="+expt+" && $2==\""+bio_system+"\" {print $3}",
+     os.path.join(os.path.expanduser('~'), "optrep/expts/info.expts")]).strip()
 
-avg_sampling_time, std_err_sampling_time = stats_helper.get_sampling_time(machine_run_on,expt)
+avg_sampling_time, std_err_sampling_time = stats_helper.get_sampling_time(
+    machine_run_on, expt)
 
-print >>out_file,"Average sampling time and std error in seconds on ",machine_run_on," : %.2f %.2f" %(float(avg_sampling_time),float(std_err_sampling_time))
+print("Average sampling time and std error in seconds on ", machine_run_on,
+      " : %.2f %.2f" % (float(avg_sampling_time),
+                        float(std_err_sampling_time)), file=out_file)
 
-# 2. Get representation precision 
-all_beads_stats,data_beads_stats,non_data_beads_stats=stats_helper.get_representation_resolution_and_precision(configs_dict["PROTEINS_TO_OPTIMIZE_LIST"],configs_dict["DOMAINS_TO_OPTIMIZE_LIST"],os.path.join(configs_dict["INPUT_DIR"],configs_dict["XLINKS_FILE"]))
+# 2. Get representation precision
+all_beads_stats, data_beads_stats, non_data_beads_stats = \
+    stats_helper.get_representation_resolution_and_precision(
+        configs_dict["PROTEINS_TO_OPTIMIZE_LIST"],
+        configs_dict["DOMAINS_TO_OPTIMIZE_LIST"],
+        os.path.join(configs_dict["INPUT_DIR"], configs_dict["XLINKS_FILE"]))
 
-print >>out_file,"All bead average resolution, diameter, weighted diameter :%.2f %.2f %.2f" %(all_beads_stats["avg_num_residues"],all_beads_stats["avg_dia"],all_beads_stats["avg_weighted_dia"])                                                                                                                               
-                                                                                                                                 
-print >>out_file,"Data bead average resolution, diameter, weighted diameter :%.2f %.2f %.2f" %(data_beads_stats["avg_num_residues"],data_beads_stats["avg_dia"],data_beads_stats["avg_weighted_dia"])
+print("All bead average resolution, diameter, weighted diameter: "
+      "%.2f %.2f %.2f" % (all_beads_stats["avg_num_residues"],
+                          all_beads_stats["avg_dia"],
+                          all_beads_stats["avg_weighted_dia"]), file=out_file)
+print("Data bead average resolution, diameter, weighted diameter: "
+      "%.2f %.2f %.2f" % (data_beads_stats["avg_num_residues"],
+                          data_beads_stats["avg_dia"],
+                          data_beads_stats["avg_weighted_dia"]), file=out_file)
 
-print >>out_file,"Non-data bead average resolution, diameter, weighted diameter :%.2f %.2f %.2f" %(non_data_beads_stats["avg_num_residues"],non_data_beads_stats["avg_dia"],non_data_beads_stats["avg_weighted_dia"])
+print("Non-data bead average resolution, diameter, weighted diameter: "
+      "%.2f %.2f %.2f" % (non_data_beads_stats["avg_num_residues"],
+                          non_data_beads_stats["avg_dia"],
+                          non_data_beads_stats["avg_weighted_dia"]),
+      file=out_file)
 
 # 3. Get fit to data
-data_type =configs_dict["GOOD_SCORING_MODEL_CRITERIA_LIST"]  # for each criterion/data set/data type
+# for each criterion/data set/data type
+data_type = configs_dict["GOOD_SCORING_MODEL_CRITERIA_LIST"]
 
-if data_type=="Crosslinks":
-    avg_distance_between_xlink_beads,std_err_distance_between_xlink_beads = stats_helper.get_fit_to_xlinks(float(configs_dict["GOOD_SCORING_MODEL_MEMBER_UPPER_THRESHOLDS_LIST"]),configs_dict["GOOD_SCORING_MODEL_KEYWORD_LIST"])
-    # TODO assuming only 1 xlink type that is related to 1 xlink file. There could be multiple in principle, though!
-    
-    print >>out_file,"Average distance and std error between xlink'ed beads:%.2f %.2f" %(float(avg_distance_between_xlink_beads),float(std_err_distance_between_xlink_beads))
+if data_type == "Crosslinks":
+    upthresh = configs_dict["GOOD_SCORING_MODEL_MEMBER_UPPER_THRESHOLDS_LIST"]
+    avg_distance_between_xlink_beads, std_err_distance_between_xlink_beads \
+        = stats_helper.get_fit_to_xlinks(
+            float(upthresh),
+            configs_dict["GOOD_SCORING_MODEL_KEYWORD_LIST"])
+    # TODO assuming only 1 xlink type that is related to 1 xlink file.
+    # There could be multiple in principle, though!
+
+    print("Average distance and std error between xlink'ed beads: "
+          "%.2f %.2f" % (float(avg_distance_between_xlink_beads),
+                         float(std_err_distance_between_xlink_beads)),
+          file=out_file)
 
 os.chdir('good_scoring_models')
 
-sampling_precision=stats_helper.get_sampling_precision('model_sample_ids.txt',configs_dict["PROTEINS_TO_OPTIMIZE_LIST"],float(configs_dict["GRID_SIZE"]))
+sampling_precision = stats_helper.get_sampling_precision(
+    'model_sample_ids.txt', configs_dict["PROTEINS_TO_OPTIMIZE_LIST"],
+    float(configs_dict["GRID_SIZE"]))
 
-print >>out_file,"Sampling precision %.2f" %(sampling_precision)
+print("Sampling precision %.2f" % (sampling_precision), file=out_file)
 
 out_file.close()
--- utilities/plot_beadmaps_figure2.py
+++ utilities/plot_beadmaps_figure2.py
@@ -1,142 +1,158 @@
 
-import IMP
-import IMP.optrep
-import os,sys,string,math
+import os
 import argparse
-import subprocess
-import datetime
-import time
-import scipy.stats
 import stats_helper
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import numpy
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import Rectangle
-from matplotlib import gridspec
+import matplotlib.pyplot as plt  # noqa: E402
+import numpy  # noqa: E402
+from matplotlib import gridspec  # noqa: E402
 
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def parse_args():
-    
-    parser = argparse.ArgumentParser(description="Plot bead maps! Usage: plot_beadmaps.py -s 2IDO -e 9. Flag -h for more details.")
-    
-    parser.add_argument("-s","--system",dest="system",type=str,help="the name of the system")
-    
-    parser.add_argument("-e","--experiment",dest="experiment",type=str,help="the experiment number")
-    
+
+    parser = argparse.ArgumentParser(
+        description="Plot bead maps! Usage: plot_beadmaps.py -s 2IDO -e 9. "
+        "Flag -h for more details.")
+
+    parser.add_argument("-s", "--system", dest="system", type=str,
+                        help="the name of the system")
+
+    parser.add_argument("-e", "--experiment", dest="experiment", type=str,
+                        help="the experiment number")
+
     result = parser.parse_args()
- 
+
     return result
-    
-def get_xlinked_residues(xlink_file,proteins_optimized):
-    
-    xlinked_res= []
-    xlf = open(xlink_file,'r')
+
+
+def get_xlinked_residues(xlink_file, proteins_optimized):
+
+    xlinked_res = []
+    xlf = open(xlink_file, 'r')
     for ln in xlf.readlines():
-        
+
         if ln.startswith("res1"):
             continue
-            
-        [res1,prot1,res2,prot2]=ln.strip().split(',')
-        
-        if prot1 in proteins_optimized and not int(res1) in xlinked_res: #TODO for multiple proteins ideally xlinked_res is a dictionary keyed by protein name!
+
+        [res1, prot1, res2, prot2] = ln.strip().split(',')
+
+        # TODO for multiple proteins ideally xlinked_res is a dictionary
+        # keyed by protein name!
+        if prot1 in proteins_optimized and int(res1) not in xlinked_res:
             xlinked_res.append(int(res1))
-        if prot2 in proteins_optimized and not int(res2) in xlinked_res:
+        if prot2 in proteins_optimized and int(res2) not in xlinked_res:
             xlinked_res.append(int(res2))
-        
+
     return sorted(xlinked_res)
-    
+
     xlf.close()
-    
-def get_beadwise_precisions(precisions_file,proteins_optimized):
-    
+
+
+def get_beadwise_precisions(precisions_file, proteins_optimized):
+
     beadwise_precisions = {}
-    pf=open(precisions_file,'r')
+    pf = open(precisions_file, 'r')
     for ln in pf.readlines():
-        fields=ln.strip().split()
-        
+        fields = ln.strip().split()
+
         if not fields[0] in proteins_optimized:
             continue
-        
-        beadwise_precisions[int(fields[2])]=float(fields[3])
-       
+
+        beadwise_precisions[int(fields[2])] = float(fields[3])
+
     pf.close()
-     
+
     return beadwise_precisions
-    
-def generate_precisions_per_residue(precision_file,beadmap_file,proteins_optimized):
-    
-    beadwise_precisions = get_beadwise_precisions(precision_file,proteins_optimized)
-    
-    precision_per_residue=[] # contiguous range
-    
-    residue_range=[] # sequence of residues
-    
-    bead_boxes=[]
-    
-    bmf=open(beadmap_file,'r')
-    
-    bead_count=0
-    
+
+
+def generate_precisions_per_residue(precision_file, beadmap_file,
+                                    proteins_optimized):
+
+    beadwise_precisions = get_beadwise_precisions(precision_file,
+                                                  proteins_optimized)
+
+    precision_per_residue = []  # contiguous range
+
+    residue_range = []  # sequence of residues
+
+    bead_boxes = []
+
+    bmf = open(beadmap_file, 'r')
+
+    bead_count = 0
+
     for ln in bmf.readlines():
-        
-        fields=ln.strip().split()
+
+        fields = ln.strip().split()
         if not fields[0] in proteins_optimized:
             continue
-        
+
         bead_start = int(fields[2])
         bead_end = int(fields[3])
-        
-        for i in range(bead_start,bead_end+1):
+
+        for i in range(bead_start, bead_end+1):
             residue_range.append(i)
-        
-            precision_per_residue.append(beadwise_precisions[bead_count])    
-        
-        #bead_boxes.append(Rectangle((xrect,0.0),rect_width,1.0))
+
+            precision_per_residue.append(beadwise_precisions[bead_count])
+
         bead_boxes.append(bead_end+1)
-        
-        bead_count+=1
+
+        bead_count += 1
 
     bmf.close()
-    
-    precision_per_residue=numpy.expand_dims(numpy.array(precision_per_residue),axis=0)
-    
-    
-    return (precision_per_residue,residue_range,bead_boxes)
-                
+
+    precision_per_residue = numpy.expand_dims(
+        numpy.array(precision_per_residue), axis=0)
+
+    return precision_per_residue, residue_range, bead_boxes
+
+
 def plot_beadmaps():
     # Authors: Shruthi Viswanath
-     
-    # process input
-    arg=parse_args()
-  
-    data_dir = os.path.join(os.path.expanduser('~'),"optrep","expts/expt"+arg.experiment,arg.system)
-    
-    config_file = os.path.join(os.path.expanduser('~'),"optrep","input",arg.system,arg.system+".config."+arg.experiment)
-    
-    config_params = stats_helper.parse_config_file(config_file)
-    
-    xlinked_residues = get_xlinked_residues(os.path.join(os.path.expanduser('~'),"optrep","input",arg.system,config_params["XLINKS_FILE"][0]),config_params["PROTEINS_TO_OPTIMIZE_LIST"])
-   
-    precision_residues = {}
-    bead_boxes={}
-    
-    min_precision=10000
-    max_precision=0
-                    
-    for ires,resolution in enumerate(config_params["RESOLUTIONS_LIST"]): 
-        
-        precision_file = os.path.join(data_dir,"r"+resolution,"bead_precisions_"+resolution+".txt")
-        
-        beadmap_file = os.path.join(data_dir,"r"+resolution,"bead_map_"+resolution+".txt")
-        
-        precision_residues[resolution],residue_range,bead_boxes[resolution]= generate_precisions_per_residue(precision_file,beadmap_file, config_params["PROTEINS_TO_OPTIMIZE_LIST"])
 
-        min_precision=min(min_precision,min(precision_residues[resolution][0]))
-        max_precision=max(max_precision,max(precision_residues[resolution][0]))
-    
+    # process input
+    arg = parse_args()
+
+    data_dir = os.path.join(os.path.expanduser('~'), "optrep",
+                            "expts/expt"+arg.experiment, arg.system)
+
+    config_file = os.path.join(os.path.expanduser('~'), "optrep", "input",
+                               arg.system,
+                               arg.system+".config."+arg.experiment)
+
+    config_params = stats_helper.parse_config_file(config_file)
+
+    xlinked_residues = get_xlinked_residues(
+        os.path.join(os.path.expanduser('~'), "optrep", "input",
+                     arg.system, config_params["XLINKS_FILE"][0]),
+        config_params["PROTEINS_TO_OPTIMIZE_LIST"])
+
+    precision_residues = {}
+    bead_boxes = {}
+
+    min_precision = 10000
+    max_precision = 0
+
+    for ires, resolution in enumerate(config_params["RESOLUTIONS_LIST"]):
+
+        precision_file = os.path.join(data_dir, "r"+resolution,
+                                      "bead_precisions_"+resolution+".txt")
+
+        beadmap_file = os.path.join(data_dir, "r"+resolution,
+                                    "bead_map_"+resolution+".txt")
+
+        (precision_residues[resolution],
+         residue_range, bead_boxes[resolution]) = \
+            generate_precisions_per_residue(
+                precision_file, beadmap_file,
+                config_params["PROTEINS_TO_OPTIMIZE_LIST"])
+
+        min_precision = min(min_precision,
+                            min(precision_residues[resolution][0]))
+        max_precision = max(max_precision,
+                            max(precision_residues[resolution][0]))
+
     # Get simple bar info from xlinks, to show along sequence
     xlink_bars = []
     for r in residue_range:
@@ -144,93 +160,108 @@ def plot_beadmaps():
             xlink_bars.append(0.5)
         else:
             xlink_bars.append(0.0)
-         
-         
+
     # changing the font sizes of axes and so on
     # need to put it in the front of the file
-    matplotlib.rcParams["xtick.labelsize"] = 24 
-     
-    # for getting colorbar  
-    #fig = plt.figure(figsize=(15, 4))
-    #gs = gridspec.GridSpec(15, 1)
+    matplotlib.rcParams["xtick.labelsize"] = 24
+
+    # for getting colorbar
+    # fig = plt.figure(figsize=(15, 4))
+    # gs = gridspec.GridSpec(15, 1)
 
     fig = plt.figure(figsize=(14, 4))
     gs = gridspec.GridSpec(12, 1)
 
     # first add the xlinks to show where the data is along the sequence
-    #ax1 = fig.add_subplot(7,1,1)
-    ax1 = fig.add_subplot(gs[0,0])
-    ax1.bar(residue_range,xlink_bars,width=1.0,align='edge',color='black')
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax1.bar(residue_range, xlink_bars, width=1.0, align='edge', color='black')
     ax1.set_xlim(min(residue_range), max(residue_range)+1)
     ax1.set_xticks([])
     ax1.set_yticks([])
-    
+
     # get an empty subplot
-    #ax2 = fig.add_subplot(7,1,2)
-    ax2  = fig.add_subplot(gs[1,0])
+    ax2 = fig.add_subplot(gs[1, 0])
     ax2.set_visible(False)
-        
-    #ax3 = fig.add_subplot(7,1,3)
-    ax3 = fig.add_subplot(gs[2:4,0])
-    ax3.imshow(precision_residues['1'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
-    ax3.vlines(bead_boxes['1'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=1.5)
+
+    ax3 = fig.add_subplot(gs[2:4, 0])
+    ax3.imshow(
+        precision_residues['1'], cmap='hot', interpolation='nearest',
+        aspect='auto',
+        extent=[min(residue_range), max(residue_range)+1, 0, 0.5],
+        norm=matplotlib.colors.Normalize(vmin=min_precision,
+                                         vmax=max_precision))
+    ax3.vlines(bead_boxes['1'], ymin=0, ymax=0.5, colors='black',
+               linestyles='dotted', linewidth=1.5)
     ax3.set_xlim(min(residue_range), max(residue_range)+1)
     ax3.set_xticks([])
     ax3.set_yticks([])
-    
+
     # Make sure vlines, precisions and the x axes are all sync'ed
-    
-    #normalize precision over full heatmap. Can you compare precisions of different runs? You cannot but the numerical values need to be compared, so yes. Make it uniform. 
-    
-    #ax4 = fig.add_subplot(7,1,4)
-    ax4 = fig.add_subplot(gs[4:6,0])
-    ax4.imshow(precision_residues['5'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
-    ax4.vlines(bead_boxes['5'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=1.5)
+
+    # normalize precision over full heatmap. Can you compare precisions
+    # of different runs? You cannot but the numerical values need to be
+    # compared, so yes. Make it uniform.
+
+    ax4 = fig.add_subplot(gs[4:6, 0])
+    ax4.imshow(
+        precision_residues['5'], cmap='hot', interpolation='nearest',
+        aspect='auto',
+        extent=[min(residue_range), max(residue_range)+1, 0, 0.5],
+        norm=matplotlib.colors.Normalize(vmin=min_precision,
+                                         vmax=max_precision))
+    ax4.vlines(bead_boxes['5'], ymin=0, ymax=0.5, colors='black',
+               linestyles='dotted', linewidth=1.5)
     ax4.set_xlim(min(residue_range), max(residue_range)+1)
     ax4.set_xticks([])
     ax4.set_yticks([])
-    
-    #ax5 = fig.add_subplot(7,1,5)
-    ax5 = fig.add_subplot(gs[6:8,0])
-    ax5.imshow(precision_residues['10'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
-    ax5.vlines(bead_boxes['10'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=1.5)
+
+    ax5 = fig.add_subplot(gs[6:8, 0])
+    ax5.imshow(
+        precision_residues['10'], cmap='hot', interpolation='nearest',
+        aspect='auto',
+        extent=[min(residue_range), max(residue_range)+1, 0, 0.5],
+        norm=matplotlib.colors.Normalize(vmin=min_precision,
+                                         vmax=max_precision))
+    ax5.vlines(bead_boxes['10'], ymin=0, ymax=0.5, colors='black',
+               linestyles='dotted', linewidth=1.5)
     ax5.set_xlim(min(residue_range), max(residue_range)+1)
     ax5.set_xticks([])
     ax5.set_yticks([])
-  
-   
-    #ax6 = fig.add_subplot(7,1,6)
-    ax6 = fig.add_subplot(gs[8:10,0])
-    ax6.imshow(precision_residues['20'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
-    ax6.vlines(bead_boxes['20'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=1.5)
+
+    ax6 = fig.add_subplot(gs[8:10, 0])
+    ax6.imshow(
+        precision_residues['20'], cmap='hot', interpolation='nearest',
+        aspect='auto',
+        extent=[min(residue_range), max(residue_range)+1, 0, 0.5],
+        norm=matplotlib.colors.Normalize(vmin=min_precision,
+                                         vmax=max_precision))
+    ax6.vlines(bead_boxes['20'], ymin=0, ymax=0.5, colors='black',
+               linestyles='dotted', linewidth=1.5)
     ax6.set_xlim(min(residue_range), max(residue_range)+1)
     ax6.set_xticks([])
     ax6.set_yticks([])
-   
-    #ax7 = fig.add_subplot(7,1,7)    
-    ax7 = fig.add_subplot(gs[10:12,0])
-    color_image = ax7.imshow(precision_residues['30'],cmap='hot',interpolation='nearest',aspect='auto',extent=[min(residue_range),max(residue_range)+1,0,0.5],norm=matplotlib.colors.Normalize(vmin=min_precision,vmax=max_precision))
-    ax7.vlines(bead_boxes['30'],ymin=0,ymax=0.5,colors='black',linestyles='dotted',linewidth=1.5)
+
+    ax7 = fig.add_subplot(gs[10:12, 0])
+    ax7.imshow(
+        precision_residues['30'], cmap='hot',
+        interpolation='nearest', aspect='auto',
+        extent=[min(residue_range), max(residue_range)+1, 0, 0.5],
+        norm=matplotlib.colors.Normalize(vmin=min_precision,
+                                         vmax=max_precision))
+    ax7.vlines(bead_boxes['30'], ymin=0, ymax=0.5, colors='black',
+               linestyles='dotted', linewidth=1.5)
     ax7.set_xlim(min(residue_range), max(residue_range)+1)
     ax7.set_xticks(numpy.arange(min(residue_range), max(residue_range)+1, 20))
     ax7.set_yticks([])
-    
-    # this is for getting the color bar only
-    ## empty subplot
-    #ax8  = fig.add_subplot(gs[12:14,0])
-    #ax8.set_visible(False)
-    
-    #ax9 = fig.add_subplot(gs[14:15,0])
-    #cbar = fig.colorbar(color_image, cax=ax9, ticks=[min_precision, (min_precision+max_precision)/2.0, max_precision], orientation='horizontal',fraction=1.5,shrink=0.5,pad=1.0,aspect=100)
-    #cbar.set_ticklabels(['High', 'Medium', 'Low'])  # horizontal colorbar
-    
+
     plt.tight_layout()
-   
+
     # remove the space between subplots
-    plt.subplots_adjust(wspace=0.01,hspace=0.01) 
+    plt.subplots_adjust(wspace=0.01, hspace=0.01)
     # note that this must be called AFTER any call to tight_layout
-    
-    plt.savefig("beadheatmap_"+arg.system+".pdf",dpi=600)
-      
-if __name__ == "__main__" :
+
+    plt.savefig("beadheatmap_"+arg.system+".pdf", dpi=600)
+
+
+if __name__ == "__main__":
     plot_beadmaps()
